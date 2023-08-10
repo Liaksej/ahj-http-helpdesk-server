949eac565b6a4efba721117ad8091c9a5018f1c5
Liaksej <tarusov.liaksej@gmail.com>
2023-08-10 21:52:23 +0200

    "Add CORS middleware and modify listening port
    The commit introduces the CORS middleware to the Koa server which allows the server to handle cross-origin requests. This resolves issues which clients may have experienced while making requests from different origins. Furthermore, the server listening port was changed from 8080 to 8081 to avoid conflicts with other running services."

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const Koa = require("koa");
 const bodyParser = require("koa-bodyparser");
+const cors = require("@koa/cors");
 const Ticket = require("./modules/Ticket");
-
-const app = new Koa();
 
 const tickets = [];
 const ticketsFull = [];
+
+const app = new Koa();
+app.use(cors());
 
 app.use(async (ctx, next) => {
   if (ctx.request.method !== "OPTIONS") {
@@ -66,4 +68,4 @@ app.use(async (ctx) => {
   }
 });
 
-app.listen(8080);
+app.listen(8081);
